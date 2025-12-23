d27402e6d8f36fdcf5442c38fb48a563d09ffb30
k0heik <alcak1729@gmail.com>
2025-12-23 17:49:58 +0900

    MakefileとTerraform設定の修正: IMAGE_NAMEとDOCKER_TAGの変数を導入し、docker_build_commandを簡素化

--- automator/infrastructure/run.tf
+++ automator/infrastructure/run.tf
@@ -6,8 +6,8 @@ module "cloud_run" {
   environment                    = var.environment
   system                         = var.system
   image_name                     = "podcast-automator"
-  docker_context_path            = "${path.module}/.."
-  docker_build_command           = "make -C app docker-build"
+  docker_context_path            = "${path.module}/../app"
+  docker_build_command           = "make docker-build"
   docker_build_result_image_name = "podcast-automator:latest"
   service_account_email          = local.default_compute_service_account
 
