c59c1879660d7b04bd77ace1154b8590ad166076
k0heik <alcak1729@gmail.com>
2025-12-23 19:55:55 +0900

    GCPインフラストラクチャの更新: .terraform.lock.hclのプロバイダーのハッシュを追加し、locals.tfのコードを整形

--- automator/infrastructure/modules/gcp/artifact_registry_docker_image/locals.tf
+++ automator/infrastructure/modules/gcp/artifact_registry_docker_image/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  repository_name      = lower("${var.system}-${var.environment}")
-  repository_url       = "${var.region}-docker.pkg.dev/${var.project_id}/${local.repository_name}"
-  docker_image_latest  = "${local.repository_url}/${var.image_name}:latest"
-  docker_image         = "${local.repository_url}/${var.image_name}:${md5(data.external.docker_image_digest.result.digest)}"
+  repository_name     = lower("${var.system}-${var.environment}")
+  repository_url      = "${var.region}-docker.pkg.dev/${var.project_id}/${local.repository_name}"
+  docker_image_latest = "${local.repository_url}/${var.image_name}:latest"
+  docker_image        = "${local.repository_url}/${var.image_name}:${md5(data.external.docker_image_digest.result.digest)}"
 }
