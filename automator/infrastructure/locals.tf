389c12fe907af477c1938e0b88ba3e18d1d4a994
k0heik <alcak1729@gmail.com>
2025-12-23 10:01:12 +0900

    required_servicesを追加し、google_project_serviceリソースを実装

--- automator/infrastructure/locals.tf
+++ automator/infrastructure/locals.tf
@@ -1,3 +1,16 @@
 locals {
   project_id = lower("${var.system}-${var.environment}")
+
+  required_services = [
+    "artifactregistry.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "eventarc.googleapis.com",
+    "logging.googleapis.com",
+    "pubsub.googleapis.com",
+    "run.googleapis.com",
+    "secretmanager.googleapis.com",
+    "storage.googleapis.com",
+    "aiplatform.googleapis.com",
+  ]
 }
