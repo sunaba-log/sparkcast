af6396a8d44d16f63acc5db192b129a1788d96a1
k0heik <alcak1729@gmail.com>
2025-12-27 16:24:13 +0900

    インフラストラクチャのコード整形: regionのインデントを修正

--- automator/infrastructure/workflows.tf
+++ automator/infrastructure/workflows.tf
@@ -1,6 +1,6 @@
 resource "google_workflows_workflow" "main" {
   name                = lower("${var.system}-${var.environment}")
-  region            = var.region
+  region              = var.region
   service_account     = local.default_compute_service_account
   deletion_protection = false
 
